642a07a9aee45caf6f41b9148fe8ccfe7a4fb653
Taki <p.materia17@gmail.com>
2022-10-25 10:39:40 +0200

    Start project

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Controller.class, args);
+    }
+    private String risposta = "SI";
+
+    @RequestMapping("/index.html")
+    public String index() {
+
+        if(risposta != "NO"){
+         return risposta;
+        }else{
+            risposta = "SI";
+            return risposta;
+        }
+
+
+    }
+}

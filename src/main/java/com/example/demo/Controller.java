6b5c0fd223af6d56eb02bf20d68d23ac5a998dd1
Taki <p.materia17@gmail.com>
2022-10-25 10:44:37 +0200

    edited controller

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
     public static void main(String[] args) {
         SpringApplication.run(Controller.class, args);
     }
-    private String risposta = "SI";
+    private String risposta = "PORCO DIO";
 
     @RequestMapping("/index.html")
     public String index() {

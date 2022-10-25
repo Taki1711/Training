b4dd610519c26bcaa830a7333904cd732f0f16ef
Walter Maio <44192985+Wal93@users.noreply.github.com>
2022-10-25 11:15:33 +0200

    TODO entro 26/10/2022

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,16 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DemoApplication {
 
+    /*
+    1) Creazione DATABASE mySQL comprensiva di collegamento ad IDE e all'applicazione DemoApplication.
+
+    2) Creazione tabella Persona con attributi(nome, cognome, eta, cellulare, email).
+
+     */
+
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }

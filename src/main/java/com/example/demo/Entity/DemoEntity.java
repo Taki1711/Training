d89ff14b114c6910b2161afe3f29c30c16a49cfe
PietroMate <pietro.materia17@hotmail.com>
2023-02-10 18:37:14 +0100

    Add package and Repo

--- src/main/java/com/example/demo/Entity/DemoEntity.java
+++ src/main/java/com/example/demo/Entity/DemoEntity.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.Entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
@@ -6,12 +6,17 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Entity;
+
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name="DemoApplication")
 public class DemoEntity {
         @Id
+        @Column (name= "ID")
+        Long id;
         @Column(name = "nome")
         String nome;
         @Column(name="cognome")
